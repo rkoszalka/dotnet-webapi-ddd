02988db311bb7a4ec23b819dc2cba457daec0349
Raphael Luiz Martins Koszalka <rlkoszalka@latam.stefanini.com>
2023-09-25 03:53:22 -0300

    (feat) :added tests

--- Koszalka.Application/Features/ToDoFeatures/CreateAllToDo/CreateTodoValidator.cs
+++ Koszalka.Application/Features/ToDoFeatures/CreateAllToDo/CreateTodoValidator.cs
@@ -1,9 +1,4 @@
 ﻿using FluentValidation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 // @todo: validate with fluent or validate in the entity class?
 namespace Koszalka.Application.Features.ToDoFeatures.CreateToDoTask
@@ -12,8 +7,8 @@ namespace Koszalka.Application.Features.ToDoFeatures.CreateToDoTask
     {
         public CreateToDoValidator()
         {
-            RuleFor(x => x.Task).NotEmpty().MaximumLength(50);
-            RuleFor(x => x.Owner).NotEmpty().MinimumLength(3).MaximumLength(50);
+            RuleFor(x => x.Task).NotEmpty().MinimumLength(3).MaximumLength(150);
+            RuleFor(x => x.Owner).NotEmpty().MinimumLength(3).MaximumLength(75);
         }
     }
 }
